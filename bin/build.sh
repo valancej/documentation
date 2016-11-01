01af885145b902f1bdcd21fbe23baca9158cfd65
thoughtkettle <thoughtkettle@users.noreply.github.com>
2016-11-01 18:12:48 +0100

    Clean up bash script
    also fixed path to release notes for Docker infrastructure

--- bin/build.sh
+++ bin/build.sh
@@ -6,12 +6,12 @@ log() { echo -e "\033[36m$@\033[39m"; }
 # subdirectory, but use an empty baseurl, so we can deploy to the bucket root
 target="${CI_BRANCH}"
 baseurl="/${CI_BRANCH}"
-envname="dev"
+JEKYLL_ENV="dev"
 if [ "${CI_BRANCH}" = "master" ]; then
 	baseurl=""
-	envname="production"
-elif [ "${CI_BRANCH}" = ${staging/*} || "${CI_BRANCH}" = ${private/*} ]; then
-	envname="staging"
+	JEKYLL_ENV="production"
+elif [ "${CI_BRANCH}" = "${staging/*}" ] || [ "${CI_BRANCH}" = "${private/*}" ]; then
+	JEKYLL_ENV="staging"
 fi
 
 # Where do we want to generate the site at?
@@ -25,12 +25,13 @@ jet_source="/site/.jet"
 if [ -f "_data/jet.yml" ]; then
 	sed -i'' -e "s|^version:.*|version: $(cat ${jet_source}/version)|" "_data/jet.yml"
 fi
-if [ -f "_posts/docker/jet/2015-07-16-release-notes.md" ]; then
-	cat "${jet_source}/release-notes" >> "_posts/docker/jet/2015-07-16-release-notes.md"
+if [ -f "_docker/getting-started/release-notes.md" ]; then
+	cat "${jet_source}/release-notes" >> "_docker/getting-started/release-notes.md"
 fi
 rm -rf "${jet_source}"
 
 # Compile the site
-log "Building with base URL '${baseurl}'"
+log "Building with base URL '${baseurl}' and environment '${JEKYLL_ENV}'."
 sed -i'' -e "s|^baseurl:.*|baseurl: ${baseurl}|" _config.yml
-bundle exec JEKYLL_ENV=${envname} jekyll build --destination "${destination}"
+export JEKYLL_ENV
+bundle exec jekyll build --destination "${destination}"
