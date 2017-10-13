e31ca7ba541bc65420a4a33bea66d3e9360bdb42
Marko Locher <marko@codeship.com>
2017-10-13 17:02:37 +0200

    Implement category pages similar to tags

--- _plugins/category_pages.rb
+++ _plugins/category_pages.rb
@@ -1,28 +1,42 @@
 module Jekyll
-
   class CategoryPage < Page
-    def initialize(site, base, dir, category)
+    def initialize(site, base, dir, category, documents)
       @site = site
       @base = base
       @dir = dir
       @name = 'index.html'
 
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'category.html')
+      self.read_yaml(File.join(base, '_layouts'), 'categories.html')
       self.data['category'] = category
+			self.data['documents'] = documents
     end
   end
 
   class CategoryPageGenerator < Generator
     safe true
+		@@categories = {}
 
-    def generate(site)
-      if site.layouts.key? 'category'
-        site.categories.keys.each do |category|
-          site.pages << CategoryPage.new(site, site.source, category, category)
-        end
-      end
-    end
-  end
+		def generate(site)
+			if site.layouts.key? 'categories'
+				dir = site.config['categories_dir'] || 'categories'
+				site.collections.each do |name, collection|
+					collection.docs.each do |document|
+						next unless document.data['categories']
+						document.data['categories'].each { |category| collect_document(category, document) }
+					end
+				end
 
+				@@categories.each do |category, documents|
+					site.pages << CategoryPage.new(site, site.source, File.join(dir, category.downcase.gsub(' ', '-')), category, documents)
+				end
+			end
+		end
+
+		def collect_document(category, document)
+			category = category.downcase
+			@@categories[category] = [] unless @@categories.has_key?(category)
+			@@categories[category] << document
+		end
+	end
 end
