d939180d9405d9917e3fffc663312abafcec4e58
Marko Locher <marko@codeship.com>
2018-01-15 14:13:05 +0100

    Update the build process to use the new Jet v2 Changelog

--- bin/jet.sh
+++ bin/jet.sh
@@ -1,38 +1,13 @@
 #!/bin/sh
 set -e
-log() { echo -e "\033[36m$@\033[39m"; }
+log() { echo -e "\033[36m$*\033[39m"; }
 
 # configuration
-unset $latest_version
 target="/site/.jet"
 
 log "Preparing Jet release notes and current version"
 rm -rf "${target}" && mkdir -p "${target}/sync"
-aws s3 sync "s3://${JET_RELEASE_BUCKET}/" "${target}/sync" --exclude "*" --include "*.changelog" --quiet
-
-# write the release notes
-find "${target}/sync/" -name "*.changelog" -type f -not -size 0 | natsort -r | \
-while read line; do
-	version=$(basename ${line} | sed -e 's/\.changelog//')
-	if [ -z "${latest_version}" ]; then
-		# set the latest version on the first iteration of the while loop
-		latest_version=${version}
-		echo "${latest_version}" > "${target}/version"
-	fi
-	cat >> "${target}/release-notes" <<-EOF
-
-		## ${version}
-
-		$(cat ${line})
-	EOF
-done
-
-# TODO
-# surround "{{...}}" tags with {% raw %}...{% endrawr %} as those are used for
-# both go templates as well as the Jekyll templating system
-
-# cleanup
-rm -rf "${target}/sync"
+aws s3 sync "s3://${JET_RELEASE_BUCKET}/latest/" "${target}/"
 
 # print the latest version
 log "Latest version is $(cat "${target}/version")"
